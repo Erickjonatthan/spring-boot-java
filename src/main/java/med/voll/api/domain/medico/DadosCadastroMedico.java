b47297f715b73b087dbe2edfc5b7877d0625586f
Erick Jonathan <zxjonathanxz22@gmail.com>
2023-11-29 21:00:16 -0300

    Update DadosCadastroMedico.java

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
-import med.voll.api.domain.endereco.Endereco;
-
 
 public record DadosCadastroMedico(
 
